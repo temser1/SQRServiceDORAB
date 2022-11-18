2eb15d2ddc3d925fdd063c27ed35f7d5618fdd83
Serovtsev Artem <1doncarleon@gmail.com>
2022-11-18 20:43:13 +0300

    Add file

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,48 +2,14 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int calculate(int min, int max) {
+    public int calculator(int min, int max) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
-            int ii = i * i;
-            if (ii >= min) {
-                if (ii <= max) {
+            if (i * i >= min) {
+                if (i * i <= max) {
                     counter++;
-
                 }
-
             }
-
-        }
-        return counter;
-    }
-    public int calculate1(int min, int max) {
-        int counter = 0;
-        for (int i = 1; i <= 10; i++) {
-            int ii = i * i;
-            if (ii >= min) {
-                if (ii <= max) {
-                    counter++;
-
-                }
-
-            }
-
-        }
-        return counter;
-    }
-    public int calculate2(int min, int max) {
-        int counter = 0;
-        for (int i = 0; i <= 50; i++) {
-            int ii = i * i;
-            if (ii >= min) {
-                if (ii <= max) {
-                    counter++;
-
-                }
-
-            }
-
         }
         return counter;
     }
